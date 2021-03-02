c0b734b74f423e7c2798feead8998289b6fecffd
Thomas Stromberg <t+github@stromberg.org>
2021-03-02 09:07:40 -0800

    Bump github library from v31 to v33

--- pkg/hubbub/issue.go
+++ pkg/hubbub/issue.go
@@ -23,7 +23,7 @@ import (
 	"github.com/google/triage-party/pkg/constants"
 	"github.com/google/triage-party/pkg/provider"
 
-	"github.com/google/go-github/v31/github"
+	"github.com/google/go-github/v33/github"
 	"github.com/google/triage-party/pkg/logu"
 	"gopkg.in/yaml.v2"
 	"k8s.io/klog/v2"
